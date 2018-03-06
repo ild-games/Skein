05522e1f340b8d49829e3ba40d4b705aebc6722b
Tucker Lein <self@tuckerle.in>
2018-03-05 19:47:28 -0600

    integrate code coverage with code climate

--- src/spool-tests/test.spec.ts
+++ src/spool-tests/test.spec.ts
@@ -1,7 +1,7 @@
-import 'jasmine';
+import { expect } from 'chai';
 
 describe("test that unit tests are being picked up and ran", function () {
     it("runs a test unit test", function () {
-        expect(true).toBe(true);
+        expect(true).to.eql(true);
     })
 });
